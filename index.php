5e61744ef9c943c348533f6354e29c41fda80b36
Luiz Fernando Costa <luiz_batera_9@hotmail.com>
2026-03-12 19:41:50 -0300

    update

--- index.php
+++ index.php
@@ -4,38 +4,10 @@ if($_SERVER["REQUEST_METHOD"] == "GET") {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST['email'];
-    
-
-    $arquivo = $_FILES['imagem'];
-
-    if ($arquivo['error'] === 0) {
-        $nomeOriginal = $arquivo['name'];
-        $tipo = $arquivo['type'];
-        $tamanho = $arquivo['size']; 
-        $tmpNome = $arquivo['tmp_name']; 
-
-        echo "<h3>Upload de Teste Recebido!</h3>";
-        echo "E-mail: " . htmlspecialchars($email) . "<br>";
-        echo "Arquivo: " . $nomeOriginal . "<br>";
-        echo "Tipo: " . $tipo . "<br>";
-        echo "Tamanho: " . ($tamanho / 1024) . " KB<br>";
-
-      
-        if (!is_dir('uploads')) {
-            mkdir('uploads', 0777, true);
-        }
-
-        $destino = "uploads/" . $nomeOriginal;
-
-        if (move_uploaded_file($tmpNome, $destino)) {
-            echo "<br><strong>Status:</strong> Arquivo salvo em /uploads com sucesso!";
-            echo "<br><img src='$destino' width='200'>";
-        } else {
-            echo "Erro ao mover o arquivo.";
-        }
-    } else {
-        echo "Erro no upload: " . $arquivo['error'];
+    if(isset($_POST["v_email"])){
+        echo "A informacao enviada foi " . $_POST["v_email"];
+    }else{
+        echo "Alguma coisa deu errado!";
     }
 }
 ?>
